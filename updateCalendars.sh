9e977723f0e4b323997e430102baba74ee2f2f44
Lukas Karlsson <karlsson@broadinstitute.org>
2015-07-29 00:43:38 -0400

    Updated to add bits calendar and have output.

--- updateCalendars.sh
+++ updateCalendars.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# add the BITS calendar for all BITS group members
+echo "Adding BITS calendar to all bits group members..."
 ./gam group bits@broadinstitute.org add calendar broadinstitute.com_5tpq5qeidntrt7teesabug75rg@group.calendar.google.com hidden false
 
-# add the Broad Institute calendar for all active Broadies
-./gam group bits@broadinstitute.org add calendar broadinstitute.com_qgfmvoepcpsfkslo4e146ntt4s@group.calendar.google.com selected true hidden false colorindex 16
+echo "Adding Broad Institute calendar to all activebroadies group members..."
+./gam group activebroadies@broadinstitute.org add calendar broadinstitute.com_qgfmvoepcpsfkslo4e146ntt4s@group.calendar.google.com selected true hidden false colorindex 16
+
