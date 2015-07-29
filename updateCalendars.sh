83b1bb0fe6860aa5295b306a1985891dfee8a229
Lukas Karlsson <karlsson@broadinstitute.org>
2015-07-29 00:40:05 -0400

    Added a new script to update add the broad institute calendar for all broadies.

---
+++ updateCalendars.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# add the BITS calendar for all BITS group members
+./gam group bits@broadinstitute.org add calendar broadinstitute.com_5tpq5qeidntrt7teesabug75rg@group.calendar.google.com hidden false
+
+# add the Broad Institute calendar for all active Broadies
+./gam group bits@broadinstitute.org add calendar broadinstitute.com_qgfmvoepcpsfkslo4e146ntt4s@group.calendar.google.com selected true hidden false colorindex 16
