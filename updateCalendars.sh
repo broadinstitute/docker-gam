7214e4ac6c9809780be4a9c29118f594d70d8f75
Lukas Karlsson <karlsson@broadinstitute.org>
2015-07-29 00:48:43 -0400

    Changed bits group to bits-all.

--- updateCalendars.sh
+++ updateCalendars.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Adding BITS calendar to all bits group members..."
-./gam group bits@broadinstitute.org add calendar broadinstitute.com_5tpq5qeidntrt7teesabug75rg@group.calendar.google.com hidden false
+./gam group bits-all@broadinstitute.org add calendar broadinstitute.com_5tpq5qeidntrt7teesabug75rg@group.calendar.google.com hidden false
 
 echo "Adding Broad Institute calendar to all activebroadies group members..."
 ./gam group activebroadies@broadinstitute.org add calendar broadinstitute.com_qgfmvoepcpsfkslo4e146ntt4s@group.calendar.google.com selected true hidden false colorindex 16
